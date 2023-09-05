cd116fe0bc98a10ac19e8717367ff518cd2f2183
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 12:37:33 +0300

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,30 @@
 def write_file(file_name, file_content):
-    pass
+    try:
+        with open(f"{file_name}.txt", mode="w") as file_open:
+            file_open.write(file_content)
+        print(f'Successfully wrote to "{file_name}.txt".')
+    except Exception as e:
+        print(f'Error writing to file: {e}')
 
 def append_file(file_name, append_content):
-    pass
+    try:
+        with open(f"{file_name}.txt", mode="a") as file_append:
+            file_append.write(append_content)
+        print(f'Successfully appended to "{file_name}.txt".')
+    except Exception as e:
+        print(f'Error appending to file: {e}')
 
 def read_file(file_name):
-    pass
+    try:
+        with open(f"{file_name}.txt", mode="r") as open_file:
+            return open_file.read()
+    except FileNotFoundError:
+        print(f'File not found: "{file_name}.txt"')
+    except Exception as e:
+        print(f'Error reading file: {e}')
+
+# Example usage:
+# write_file("example", "Hello, World!")
+# append_file("example", "\nAppended Text.")
+# content = read_file("example")
+# print(content)
